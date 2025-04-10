5a0ad53bfa7077a2486b2baafbad7f6f7be05578
Anand Fursule <anand.fursale@onerooftech.com>
2025-04-10 18:39:51 +0530

    added contact and tags moduls

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,33 @@ document.addEventListener("DOMContentLoaded", function () {
     updateStep();
 });
 
+
+// Filter Dropdown code
+
+function setupSearchAndCheckboxBehavior(searchInputId, listId) {
+    const searchInput = document.getElementById(searchInputId);
+    const checkboxes = document.querySelectorAll(`#${listId} .form-check-input`);
+
+    // Filter the checkbox list based on the search input
+    searchInput.addEventListener('input', function () {
+        const searchValue = this.value.toLowerCase().trim();
+        checkboxes.forEach(checkbox => {
+            const label = checkbox.nextElementSibling.textContent.toLowerCase();
+            checkbox.parentElement.style.display = label.includes(searchValue) ? '' : 'none';
+        });
+    });
+
+    // Clear the search input when any checkbox is selected
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            searchInput.value = '';
+        });
+    });
+}
+
+// Setup for all the search and checkbox combinations
+
+setupSearchAndCheckboxBehavior('tagSearch', 'tagList');
+setupSearchAndCheckboxBehavior('citySearch', 'cityList');
+setupSearchAndCheckboxBehavior('referenceSearch', 'referenceList');
+setupSearchAndCheckboxBehavior('EngineSearch', 'EngineList');
